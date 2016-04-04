5289f274e76fcb0ec82ede99159aafe3f9938fbb
Elton Santos <elton@r2a.net.br>
2016-04-04 17:42:42 -0300

    Finish perfil part

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,4 +4,12 @@ class Profile < ActiveRecord::Base
   has_many :functionalities, through: :profile_functionalities
   belongs_to :manager
   belongs_to :agent
+
+  #
+  def validate_functionality
+    if self.functionalities.any?{|f| all_functionalities.exclude?(f) }
+      self.errors.add(:base, "Não pode criar funcionalidades")
+    end
+  end
+
 end
